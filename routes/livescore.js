e5276ffb28b62fa2fa470e653a89e954520d68aa
Prabhat Raushan <prabhatraush@gmail.com>
2020-09-11 17:06:18 +0530

    router livescore

--- routes/livescore.js
+++ routes/livescore.js
@@ -18,14 +18,12 @@ router.get('/livescore', async(req, res, next) =>{
     // res.status(200).jsonp({
         
     // })
-
-    try {
-        let response = await axios.get('https://mapps.cricbuzz.com/cbzios/match/livematches');
-        res.jsonp({data: response.data});
-    } catch (err) {
-        next(err);
-    }
-
+        await axios.get('https://mapps.cricbuzz.com/cbzios/match/livematches')
+        .then(response =>{
+            res.jsonp({data: response.data});
+        }). catch (err =>{
+            next(err);
+        }); 
 });
 
 router.get('/commentary/:mid', async(req, res, next) =>{
