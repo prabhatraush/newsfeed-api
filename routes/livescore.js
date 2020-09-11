6b8d072d933f3d52d322af2b6de40e45e299c213
Prabhat Raushan <prabhatraush@gmail.com>
2020-09-11 13:46:06 +0530

    fixing promising with try and catch

--- routes/livescore.js
+++ routes/livescore.js
@@ -7,60 +7,60 @@ router.get('/', (req, res) =>{
 })
 
 
-router.get('/livescore', async(req, res) =>{
+router.get('/livescore', async(req, res, next) =>{
 
-    const response = await axios.get('https://mapps.cricbuzz.com/cbzios/match/livematches')
-    .catch(error => { throw error});
-    if(!response) {
-        res.send({message:"Invalid Url"});
+    // const response = await axios.get('https://mapps.cricbuzz.com/cbzios/match/livematches')
+    // .catch(error => { throw error});
+    // if(!response) {
+    //     res.send({message:"Invalid Url"});
+    // }
+    // //console.log(response.data);
+    // res.status(200).send({
+        
+    // })
+
+    try {
+        let response = await axios.get('https://mapps.ricbuzz.com/cbzios/match/livematches')
+        res.send({data: response.data})
+    } catch (err) {
+        next(err);
     }
-    //console.log(response.data);
-    res.status(200).send({
-        matches: response.data
-    })
 
 });
 
-router.get('/commentary/:mid', async(req, res) =>{
+router.get('/commentary/:mid', async(req, res, next) =>{
 
     const mid = req.params.mid;
-    const response = await axios.get('https://mapps.cricbuzz.com/cbzios/match/'+mid+'/commentary')
-    .catch(error => { throw error});
-    if(!response) {
-        res.send({message:"Invalid Url"});
+    try {
+        let response = await axios.get('https://mapps.cricbuzz.com/cbzios/match/'+mid+'/commentary')
+        res.send({data: response.data})
+    } catch (err) {
+        next(err);
     }
-    //console.log(response.data);
-    res.status(200).send({
-        matches: response.data
-    })
 });
 
-router.get('/scorecard/:mid', async(req, res) =>{
+router.get('/scorecard/:mid', async(req, res, next) =>{
 
     const mid = req.params.mid;
-    const response = await axios.get('https://mapps.cricbuzz.com/cbzios/match/'+mid+'/scorecard')
-    .catch(error => { throw error});
-    if(!response) {
-        res.send({message:"Invalid Url"});
+    
+    try {
+        let response = await axios.get('https://mapps.cricbuzz.com/cbzios/match/'+mid+'/scorecard')
+        res.send({data: response.data})
+    } catch (err) {
+        next(err);
     }
-    //console.log(response.data);
-    res.status(200).send({
-        matches: response.data
-    })
+
 });
 
-router.get('/highlights/:mid', async(req, res) =>{
+router.get('/highlights/:mid', async(req, res, next) =>{
 
     const mid = req.params.mid;
-    const response = await axios.get('https://mapps.cricbuzz.com/cbzios/match/'+mid+'/highlights')
-    .catch(error => { throw error});
-    if(!response) {
-        res.send({message:"Invalid Url"});
+    try {
+        let response = await axios.get('https://mapps.cricbuzz.com/cbzios/match/'+mid+'/highlights')
+        res.send({data: response.data})
+    } catch (err) {
+        next(err);
     }
-    //console.log(response.data);
-    res.status(200).send({
-        matches: response.data
-    })
 });
 
 module.exports=router;
